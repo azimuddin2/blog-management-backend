4c939ee3ba0d75dbe25dafa88fef14883473a6a8
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-12-19 23:18:52 +0600

    database connect

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,14 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 
-const app: Application = express()
-const port = 5000
+const app: Application = express();
 
 // parsers
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req:Request, res:Response) => {
-    res.send('Blog Management Backend Software Application!')
-})
+app.get('/', (req: Request, res: Response) => {
+  res.send('Blog Management Backend Software Application!');
+});
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
+import { Server } from 'http';
+
+let server: Server;
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log('Connection to database is successfully established');
+
+    server = app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+main();
