235c9ddbdf367daf12547c92f9c9291430dc6fd4
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-12-20 23:53:52 +0600

    added QueryBuilder and validateRequest

--- src/app/modules/blog/blog.route.ts
+++ src/app/modules/blog/blog.route.ts
@@ -1,12 +1,22 @@
 import express from 'express';
 import { BlogControllers } from './blog.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { BlogValidations } from './blog.validation';
 
 const router = express.Router();
 
-router.post('/', BlogControllers.createBlog);
+router.post(
+  '/',
+  validateRequest(BlogValidations.createBlogValidationSchema),
+  BlogControllers.createBlog,
+);
 router.get('/', BlogControllers.getAllBlogs);
 router.get('/:id', BlogControllers.getSingleBlog);
-router.patch('/:id', BlogControllers.updateBlog);
+router.patch(
+  '/:id',
+  validateRequest(BlogValidations.updateBlogValidationSchema),
+  BlogControllers.updateBlog,
+);
 router.delete('/:id', BlogControllers.deleteBlog);
 
 export const BlogRoutes = router;
