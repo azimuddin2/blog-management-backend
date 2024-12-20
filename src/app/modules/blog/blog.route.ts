d306713774761c1974816b9d2263da042509c06f
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-12-20 21:41:27 +0600

    blog route, controller and service task

--- src/app/modules/blog/blog.route.ts
+++ src/app/modules/blog/blog.route.ts
@@ -1,5 +1,12 @@
 import express from 'express';
+import { BlogControllers } from './blog.controller';
 
 const router = express.Router();
+
+router.post('/', BlogControllers.createBlog);
+router.get('/', BlogControllers.getAllBlogs);
+router.get('/:id', BlogControllers.getSingleBlog);
+router.patch('/:id', BlogControllers.updateBlog);
+router.delete('/:id', BlogControllers.deleteBlog);
 
 export const BlogRoutes = router;
--- src/app/modules/blog/blog.service.ts
+++ src/app/modules/blog/blog.service.ts
@@ -1,7 +1,46 @@
 import { TBlog } from './blog.interface';
+import { Blog } from './blog.model';
 
-const createBlogIntoDB = async (payload: TBlog) => {};
+const createBlogIntoDB = async (payload: TBlog) => {
+  const result = await Blog.create(payload);
+  return result;
+};
+
+const getAllBlogsFromDB = async () => {
+  const result = await Blog.find();
+  return result;
+};
+
+const getSingleBlogFromDB = async (id: string) => {
+  const result = await Blog.findById(id);
+  return result;
+};
+
+const updateBlogIntoDB = async (id: string, payload: Partial<TBlog>) => {
+  const isBlogExists = await Blog.findById(id);
+  if (!isBlogExists) {
+    throw new Error(`This Blog id ${id} does not exists`);
+  }
+
+  const result = await Blog.findByIdAndUpdate(id, payload, { new: true });
+
+  return result;
+};
+
+const deleteBlogFromDB = async (id: string) => {
+  const isBlogExists = await Blog.findById(id);
+  if (!isBlogExists) {
+    throw new Error(`This Blog id ${id} does not exists`);
+  }
+
+  const result = await Blog.findByIdAndDelete(id);
+  return result;
+};
 
 export const BlogServices = {
   createBlogIntoDB,
+  getAllBlogsFromDB,
+  getSingleBlogFromDB,
+  updateBlogIntoDB,
+  deleteBlogFromDB,
 };
